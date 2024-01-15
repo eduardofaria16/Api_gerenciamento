46cd08b07f76c831556d93848c037c813e61790b
Eduardo Faria <eduardo.faria@licitanet.local>
2024-01-15 17:28:05 -0300

    dia-1

---
+++ database/migrations/2024_01_15_174708_create__produto.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     * @param $bp
+     */
+    public function up(): void
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->float('valor');
+            $table->unsignedBigInteger('id_marca');
+            $table->foreign('id_marca')->references('id')->on('marcas');
+            $table->float('estoque');
+            $table->unsignedBigInteger('id_cidade');
+            $table->foreign('id_cidade')->references('id')->on('cidades');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produtos');
+    }
+};
